a5c6e94c87d2084c53e91fb132f0f62aa11495a9
azedeveloper <albin.business@yahoo.com>
2025-03-16 10:18:25 +0100

    teleport confirm c2s

--- src/main/java/com/apostles/apostlehack/modules/NoTeleportAccept.java
+++ src/main/java/com/apostles/apostlehack/modules/NoTeleportAccept.java
@@ -3,6 +3,7 @@ package com.apostles.apostlehack.modules;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.c2s.play.TeleportConfirmC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 
 import static com.apostles.apostlehack.ApostleHack.CATEGORY;
@@ -13,8 +14,8 @@ public class NoTeleportAccept extends Module {
     }
 
     @EventHandler
-    private void onReceivePacket(PacketEvent.Receive event) {
-        if (event.packet instanceof PlayerPositionLookS2CPacket) {
+    private void onSendPacket(PacketEvent.Send event) {
+        if (event.packet instanceof TeleportConfirmC2SPacket) {
             event.cancel(); // Cancels the packet to prevent teleportation
         }
     }
