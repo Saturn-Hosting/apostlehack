67fc92d3202fb5b5ce3d1210e83dc72ba5660322
Fulgur <fulgurgithub@gmail.com>
2025-03-16 10:35:24 +0100

    disconnect on teleport

--- src/main/java/com/apostles/apostlehack/modules/NoTeleportAccept.java
+++ src/main/java/com/apostles/apostlehack/modules/NoTeleportAccept.java
@@ -5,6 +5,7 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.c2s.play.TeleportConfirmC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.text.Text;
 
 import static com.apostles.apostlehack.ApostleHack.CATEGORY;
 
@@ -16,7 +17,9 @@ public class NoTeleportAccept extends Module {
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
         if (event.packet instanceof TeleportConfirmC2SPacket) {
-            event.cancel(); // Cancels the packet to prevent teleportation
+            event.cancel();
+            mc.player.networkHandler.getConnection().disconnect(Text.of("Disconnected: Teleport detected."));
+            this.toggle();
         }
     }
 }
